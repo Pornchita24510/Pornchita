d93613a2c9a1dbbd8c58efb0513f3ce15a6172fb
Pornchita24510 <24510@srithatpit.ac.th>
2025-01-17 11:28:56 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+   <p>-------การแสดงข้อความแบบหัวข้อ--------</p>
+   <div>
+        <h1>HTMLเบื้องต้น(h1)</h1>
+</div>
+<div>   
+<h2>HTMLเบื้องต้น(h2)</h2>
+<h3>HTMLเบื้องต้น(h2)</h3>
+<h4>HTMLเบื้องต้น(h2)</h4>
+<h5>HTMLเบื้องต้น(h2)</h5> 
+<h6>HTMLเบื้องต้น(h2)</h6>
+</div>
+<p>-------การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ--------</p>
+<div>
+    <b>พรชิตา อุณวงศ์</b><br>
+    <i>พรชิตา อุณวงศ์</i><br>
+    <u>พรชิตา อุณวงศ์</u><br>
+</div>
+<p><b>----การLink-----</b></p>
+<div>
+    <!--Linkหน้าเดียว-->
+    <a herf = "https://www.gogle.co.th">Gogle</a><br>
+    <div>
+</body>
+</html>
